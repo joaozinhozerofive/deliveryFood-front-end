ef95f1b49c24d60223d87248cf6ba4393cce9022
joão <joaovitormachadororato@gmail.com>
2023-09-28 20:49:29 -0300

    mmm

--- src/Pages Comum/Home/index.jsx
+++ src/Pages Comum/Home/index.jsx
@@ -21,7 +21,7 @@ export function Home(){
     const [drinkCategory, setDrinkCategory] = useState([]);
     const [dessertCategory, setDessertCategory] = useState([]);
 
-    console.log({user})
+    console.log({userr})
 
     
     useEffect(() => {
