3ab99b078ce7a5c3b5641525cb6dec7d6a8238ce
joaozinhozerofive <124640745+joaozinhozerofive@users.noreply.github.com>
2023-09-28 20:39:24 -0300

    Update index.jsx

--- src/components/Meals/index.jsx
+++ src/components/Meals/index.jsx
@@ -37,7 +37,19 @@ export function Meals ({plates, ...rest}) {
         {...rest}>
 
             {isAdmin === 1 ?  
-             (
+             
+            
+            (plates && plates.map(plate => (
+            <div key={String(plate.plate_id)} className="mealsActive">
+                <img onClick={() => handleEdit(plate.plate_id)}  id="edit"  src={buttonEdit} alt="coração favoritos" />
+                <img  onClick={() => handleDetails(plate.plate_id)} id="snack" src={`${api.defaults.baseURL}/files/${plate.img}`} alt="imagem do prato " />
+                <h1 id="snackName">{`${plate.name} >` }</h1>
+                <p>{`${plate.description}`}</p>
+                <h2>{`R$ ${plate.price}`}</h2>
+                
+               </div>)))
+            :
+                (
                 plates && plates.map(plate => (
              <div key={plate.id} className="mealsActive">
                 <img id="favorites"  src={favorites} alt="coração favoritos" />
@@ -60,21 +72,7 @@ export function Meals ({plates, ...rest}) {
 
 
             </div>))) 
-            
-            :
-            (plates && plates.map(plate => (
-            <div key={String(plate.plate_id)} className="mealsActive">
-                <img onClick={() => handleEdit(plate.plate_id)}  id="edit"  src={buttonEdit} alt="coração favoritos" />
-                <img  onClick={() => handleDetails(plate.plate_id)} id="snack" src={`${api.defaults.baseURL}/files/${plate.img}`} alt="imagem do prato " />
-                <h1 id="snackName">{`${plate.name} >` }</h1>
-                <p>{`${plate.description}`}</p>
-                <h2>{`R$ ${plate.price}`}</h2>
                 
-               </div>)))
-
-
-
-             
             }
             
         </Container>
