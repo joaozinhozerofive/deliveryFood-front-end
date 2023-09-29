c365fcd3f23a9478a3c722fc8ce94ea3fdb054d8
joão <joaovitormachadororato@gmail.com>
2023-09-28 21:42:24 -0300

    teste

--- src/Pages Comum/Home/index.jsx
+++ src/Pages Comum/Home/index.jsx
@@ -21,7 +21,7 @@ export function Home(){
     const [drinkCategory, setDrinkCategory] = useState([]);
     const [dessertCategory, setDessertCategory] = useState([]);
 
-    console.log(mealCategory)
+    console.log(isAdmin)
     
     useEffect(() => {
         async function fetchDishes(){
