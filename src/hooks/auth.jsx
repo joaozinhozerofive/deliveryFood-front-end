494a8e0ccc2eba27a70444d8260d342326ca3cfa
joão <joaovitormachadororato@gmail.com>
2023-09-28 21:45:39 -0300

    teste

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -23,7 +23,7 @@ function AuthProvider ({ children }) {
             const {token, user} = response.data;
 
 
-            const isAdmin = user.admin
+            const isAdmin = user[0].admin
 
             localStorage.setItem("deliveryFood:user", JSON.stringify(user))
             localStorage.setItem("deliveryFood:token", token)
