bb10dcec4d5c4e9149ea4269b5175a717eda8b42
joão <joaovitormachadororato@gmail.com>
2023-10-02 20:32:53 -0300

    alter isAdmin to "1"

--- src/components/Meals/index.jsx
+++ src/components/Meals/index.jsx
@@ -36,7 +36,22 @@ export function Meals ({plates, ...rest}) {
         plates = {plates}
         {...rest}>
 
-              {  plates && plates.map(plate => (
+            {isAdmin === 1 ? 
+
+            (plates && plates.map(plate => (
+            <div key={String(plate.plate_id)} className="mealsActive">
+                <img onClick={() => handleEdit(plate.plate_id)}  id="edit"  src={buttonEdit} alt="coração favoritos" />
+                <img  onClick={() => handleDetails(plate.plate_id)} id="snack" src={`${api.defaults.baseURL}/files/${plate.img}`} alt="imagem do prato " />
+                <h1 id="snackName">{`${plate.name} >` }</h1>
+                <p>{`${plate.description}`}</p>
+                <h2>{`R$ ${plate.price}`}</h2>
+                
+               </div>)))
+               :
+            
+
+             
+                plates && plates.map(plate => (
              <div key={plate.id} className="mealsActive">
                 <img id="favorites"  src={favorites} alt="coração favoritos" />
                 <img onClick={() => handleDetails(plate.plate_id)} id="snack" src={`${api.defaults.baseURL}/files/${plate.img}`} alt="imagem do prato " />
@@ -46,8 +61,19 @@ export function Meals ({plates, ...rest}) {
                 <div className="ingredients">
                 
                 </div>
+                <div id="includs">
+                    <img src={imgButtonLess} alt="botao de diminuir" />
+                    <h3>01</h3>
+                    <img src={imgButtonMore} alt="botao de incluir"  />
+                    <Button 
+                    title={"incluir"}
+                    />
+                    
+                </div>
 
-            </div>))}
+
+            </div>))
+}
             
             
         </Container>
