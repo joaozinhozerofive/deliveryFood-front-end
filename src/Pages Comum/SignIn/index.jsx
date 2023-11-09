69fc12edb21069de9c586486f6c194afaee0c81a
joão <joaovitormachadororato@gmail.com>
2023-11-08 21:03:05 -0300

    newTeste

--- src/Pages Comum/SignIn/index.jsx
+++ src/Pages Comum/SignIn/index.jsx
@@ -9,7 +9,7 @@ import { toast } from "react-toastify";
 
 export function SignIn(){
     const navigation = useNavigate()
-    const [email, setEmail] = useState("Roberto da pika grossa");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const {signIn} = useAuth();
 
